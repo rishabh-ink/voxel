5f98bffa11b065ac1b075d30b0ce5be2fe453a2b
Rishabh Rao <rishabhsrao@gmail.com>
2015-05-30 20:00:16 +0530

    Remove IIFE and app module path.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,29 +1,21 @@
-(function(window, document, undefined) {
-  "use strict";
+require.config({
+  paths: {
+    // Libraries
+    "jquery": "../../libraries/jquery/jquery",
+    "hljs": "../../libraries/highlightjs/highlight.pack",
+    // /Libraries
+  },
 
-  require.config({
-    paths: {
-      // Libraries
-      "jquery": "../../libraries/jquery/jquery",
-      "hljs": "../../libraries/highlightjs/highlight.pack",
-      // /Libraries
+  shim: {
+  }
+});
 
-      // Application
-      "app": "app",
-      // /Application
-    },
-
-    shim: {
-    }
+require(["jquery", "app"],
+function( $,        App) {
+  var app = App.create({
+    name: "voxel",
+    useHighlight: true
   });
 
-  require(["jquery", "app"],
-  function( $,        App) {
-    var app = App.create({
-      name: "voxel",
-      useHighlight: true
-    });
-
-    console.log("App, %s, with jQuery v%s says, '%s'", app.options.name, $.fn.jquery, app.greet());
-  });
-})(window, document);
+  console.log("App, %s, with jQuery v%s says, '%s'", app.options.name, $.fn.jquery, app.greet());
+});
