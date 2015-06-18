e8f6e052e82a000dfe076186874d2c759f3401a9
Rishabh Rao <rishabhsrao@gmail.com>
2015-06-18 23:52:56 +0530

    Bump to v1.2.0.

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,1 +1,1 @@
-require.config({paths:{jquery:"../../libraries/jquery/jquery"},shim:{}}),require(["jquery","voxel"],function(e,r){r.create()});
+require.config({paths:{jquery:"../../libraries/jquery/jquery.min","es5-shim":"../../libraries/es5-shim/es5-shim.min","es5-sham":"../../libraries/es5-shim/es5-sham.min"},shim:{}}),require(["jquery","es5-shim","es5-sham","voxel"],function(e,s,i,r){r.create()});
